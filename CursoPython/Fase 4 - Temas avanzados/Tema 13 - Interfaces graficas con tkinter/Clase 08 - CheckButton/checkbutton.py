a8a4ba1793a2cd57f05874b8334dee0141b4b017
JesusDiaz08 <jesus_kdm@hotmail.com>
2020-04-29 01:33:31 -0500

    Tema 13 - Interfaces Gráficas - Checkbutton :fire:

--- CursoPython/Fase 4 - Temas avanzados/Tema 13 - Interfaces graficas con tkinter/Clase 08 - CheckButton/checkbutton.py
+++ CursoPython/Fase 4 - Temas avanzados/Tema 13 - Interfaces graficas con tkinter/Clase 08 - CheckButton/checkbutton.py
@@ -7,29 +7,51 @@ Created on Wed Apr  8 16:50:31 2020
 
 from tkinter import *
 
-#Un usuario que diga, como quiere cafe
+# 6 Nos ayudará a recuperar los valores de leche y azucar
+def seleccionar():
+	cadena = ""
+	if leche.get():
+		cadena += "Con leche"
+	else:
+		cadena += "Sin leche"
+		
+	if azucar.get():
+		cadena += "y con azúcar"
+	else:
+		cadena += "y sin azúcar"
+	
+	monitor.config(text = cadena)
 
-# Configuración de la raíz
+#Un usuario que diga, como quiere cafe
+# 1. Configuración de la raíz
 root = Tk()
 root.title("Cafeteria")
 root.config(bd = 15)
 
-leche = IntVar()   # 1 - sí, 0 - no
-azucar = IntVar()  # 1 - sí, 0 - no
+# 2. Almacenan las variables de los checkbutton
+leche = IntVar()		# 1 si, 0 no
+azucar = IntVar()		# 1 si, 0 no
 
+# 4. Agregamos una imagen
+imagen = PhotoImage(file = "imagen.gif")
+Label(root, image = imagen).pack(side = "left")
 
+# 5. Una vez hecho esto, vamos a crear un marco
+# para poner allí los labels
+frame = Frame(root)
+frame.pack(side = 'right')
 
+# Segundo Paso
+Label(frame, text= '¿Cómo quieres el cafe?').pack(anchor = "w")
+# 2,3 paso - onvalue : Valor cuando está clickado
+# 2,3 paso - offvalue : Valor cuando no está clickado
+# 6 - Añadimo command = seleccionar
+Checkbutton(frame, text = 'Con leche', variable = leche, onvalue = 1, offvalue = 0, command = seleccionar).pack(anchor = "w")
+Checkbutton(frame, text = 'Con azucar', variable = azucar, onvalue = 1, offvalue = 0, command = seleccionar).pack(anchor = "w")
 
+# Ahora, vamos a recuperar los valores de leche y azucar
+monitor = Label(frame)
+monitor.pack()
 
-lbl_cafe = Label(root, text = '¿Cómo quieres el café?')
-lbl_cafe.pack()
-
-cb_leche = Checkbutton(root, text = 'Con leche', variable = leche, onvalue = 1, offvalue = 0)
-cb_leche.pack()
-
-cb_azucar = Checkbutton(root, text = 'Con azucar', variable = azucar, onvalue = 1, offvalue = 0)
-cb_azucar.pack()
-
-
-# Finalmente bucle de la aplicacion
+# 1. Finalmente bucle de la aplicacion
 root.mainloop()
